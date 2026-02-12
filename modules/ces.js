40bf49db625555a5d28024774e7e33f2f7e2b2e7
Mikatoshi <230616882+GSF-001@users.noreply.github.com>
2026-02-12 08:18:00 +0700

    Delete modules/ces.js

--- modules/ces.js
+++
@@ -1,9 +0,0 @@
-const CES = {
-  process(action = {}) {
-    if (action.type === "help") return { mood: "positive" };
-    if (action.type === "spam") return { mood: "alert" };
-    return { mood: "neutral" };
-  },
-};
-
-export default CES;
